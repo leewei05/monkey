6c63aba4016063cdf27955b13a3cfd1d3a4c238c
Lee <lee10202013@gmail.com>
2021-07-02 23:26:36 +0800

    Lexer struct for parsing input code

---
+++ lexer/lexer.go
@@ -0,0 +1,23 @@
+package lexer
+
+import "github.com/leewei05/monkey/token"
+
+type Lexer struct {
+	input string
+	// position is the current position in input(points to current char)
+	position int
+	// readPosition is the current reading position(after current char)
+	readPosition int
+	// ch is the current char under examination
+	ch byte
+}
+
+func New(input string) *Lexer {
+	return &Lexer{
+		input: input,
+	}
+}
+
+func (l *Lexer) NextToken() *token.Token {
+	return &token.Token{}
+}
