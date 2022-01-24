499e55d7c5b41b885d2979e93860b8159c0676e3
Lee <lee10202013@gmail.com>
2022-01-25 01:10:20 +0800

    read next token

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -14,11 +14,55 @@ type Lexer struct {
 }
 
 func New(input string) *Lexer {
-	return &Lexer{
+	lexer := &Lexer{
 		input: input,
 	}
+	lexer.NextToken()
+
+	return lexer
 }
 
-func (l *Lexer) NextToken() *token.Token {
-	return &token.Token{}
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREM, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+// refactor here to support Unicode and emojis
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+
+	l.position = l.readPosition
+	l.readPosition += 1
 }
